c8f58f9275885edddc350ab339f63bf2a1598d0b
uimarshall <uimarshall@gmail.com>
2021-02-25 16:52:03 +0100

    Create react app and remove unnecessary files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>react is cool</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
