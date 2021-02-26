37c0e1c6c9c3f99f73f9689911d30e5c7c6c1237
uimarshall <uimarshall@gmail.com>
2021-02-26 18:45:41 +0100

    create project file structure

---
+++ src/components/UserDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserDetails() {
+  return (
+    <div>
+      <p>details</p>
+
+    </div>
+  );
+}
+
+export default UserDetails;
---
+++ src/components/UserSearchResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserSearchResult() {
+  return (
+    <div>
+      <p>results</p>
+    </div>
+  );
+}
+
+export default UserSearchResult;
